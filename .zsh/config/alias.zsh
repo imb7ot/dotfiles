9744c5f6ba7abb67024bc2cfbf0e5662fdcc98cf
imb7ot <imb7ot@gmail.com>
2024-11-23 17:43:54 +0800

    zsh: alias of ls and grep color

--- .zsh/config/alias.zsh
+++ .zsh/config/alias.zsh
@@ -1,8 +1,11 @@
 alias dotfile='git --git-dir="$HOME/.local/share/dotfile" --work-tree="$HOME"'
 
-alias lc='ls -F --group-directories-first'
-alias ll='lc -hl'
-alias la='ll -A'
-
 alias bc='bc -q'
 alias ed='ed -p:'
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+alias lc='ls --group-directories-first'
+alias ll='lc -hl'
+alias la='ll -A'

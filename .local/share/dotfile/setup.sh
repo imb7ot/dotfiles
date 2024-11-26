8c41f9f0ecce54cd6a60a1ebf3f8ce5f1770900f
imb7ot <imb7ot@gmail.com>
2024-11-27 05:37:29 +0800

    global: submodule

--- .local/share/dotfile/setup.sh
+++ .local/share/dotfile/setup.sh
@@ -1,5 +1,5 @@
 set -eu
 git clone --bare --depth=1 https://gitlab.com/imb7ot/dotfile.git "$HOME/.local/share/dotfile/git"
 alias dotfile='git --git-dir="$HOME/.local/share/dotfile/git" --work-tree="$HOME"'
-dotfile checkout -f
 dotfile config --local status.showUntrackedFiles no
+dotfile checkout --recurse-submodules -f
